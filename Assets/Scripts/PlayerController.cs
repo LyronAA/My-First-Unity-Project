84f449aa14cfef0adce99c5d76ee9db5bbec1ce0
LyronAA <lyron_aguiar@hotmail.com>
2022-04-18 18:49:03 +0300

    Added Game functionality
    Created A Player Controller script and added a new tag.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0) // It is positive //Right button or D key
+        {
+            rigidbody2D.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0) // It is negative //Left Button or A Key
+        {
+            rigidbody2D.velocity = new Vector2(-speed, 0f);
+        }
+
+        else if (Input.GetAxis("Vertical") > 0) // It is positive //Up button or W key
+        {
+            rigidbody2D.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)// It is negative //Down button or S key
+        {
+            rigidbody2D.velocity = new Vector2(0f, -speed);
+        }
+        else if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            //stop
+            rigidbody2D.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Door")
+            Debug.Log("Level Complete!!!");
+    }
+}
